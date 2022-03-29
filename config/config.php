5426fbd4ff0f03b49a33175ae81e1d21f8207128
Luca Calcaterra <calcaterra.luca@gmail.com>
2022-03-29 20:28:27 +0200

    config for ldaprecord laravel

--- config/config.php
+++ config/config.php
@@ -138,11 +138,22 @@
                 '\LdapRecord\Laravel\LdapAuthServiceProvider',
                 '\LdapRecord\Laravel\LdapServiceProvider',
             ],
-            'config_namespace' => 'auth',
+            'config_namespace' => 'ldap',
 
             'config' => [
+                'default' => env('LDAP_CONNECTION', 'default'),
+
                 'connections' => [
-                    'default' => []
+                    'default' => [
+                        'hosts' => [env('LDAP_HOST', '127.0.0.1')],
+                        'username' => env('LDAP_USERNAME', 'cn=user,dc=local,dc=com'),
+                        'password' => env('LDAP_PASSWORD', 'secret'),
+                        'port' => env('LDAP_PORT', 389),
+                        'base_dn' => env('LDAP_BASE_DN', 'dc=local,dc=com'),
+                        'timeout' => env('LDAP_TIMEOUT', 5),
+                        'use_ssl' => env('LDAP_SSL', false),
+                        'use_tls' => env('LDAP_TLS', false),
+                    ]
                 ]
             ]
             // 'aliases' => [
